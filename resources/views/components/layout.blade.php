c3d49f20481a4a95c0d45f8f25deab0c7efdeab6
Gilangp <gilangpurnomo0505@gmail.com>
2025-02-19 00:12:54 +0700

    Perbaikan Branch

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,12 +8,12 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Welcome Laravel</title>
 </head>
-<body class="h-full"> 
+<body class="h-full">
     <div class="min-h-full">
         <x-navbar></x-navbar>
-    
+
         <x-header>{{ $title }}</x-header>
-        
+
         <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <!-- Your content -->
